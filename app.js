02af0fa5a37f9441d2d24660774e2e4a31e5ebdc
Tiago Bahiense <tgbahiense@gmail.com>
2022-07-04 12:26:51 -0300

    voltando para o estado anterior

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
 console.log("rodando o sistema de gerenciamento de jogos")
-console.log("raplicação esta se conectando ao banco de dados")
-// mais um console log de teste:
-console.log("O paulo fez esse commit sem voce ver na aula")
-console.log("Enviando para o Github")
-console.log("Testando o pull")
