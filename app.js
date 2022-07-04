a7ecbfe791a1d357cb168bdffe47205c1026069f
Tiago Bahiense <tgbahiense@gmail.com>
2022-07-04 12:15:16 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("rodando o sistema de gerenciamento de jogos")
+console.log("raplicação esta se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("O paulo fez esse commit sem voce ver na aula")
+console.log("Enviando para o Github")
