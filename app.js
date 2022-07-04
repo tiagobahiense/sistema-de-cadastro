23f4db23088b63c82af18aacb29951ad06ea95b0
Tiago Bahiense <108610480+tiagobahiense@users.noreply.github.com>
2022-07-04 12:19:19 -0300

    testando o comando pull

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("raplicação esta se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("O paulo fez esse commit sem voce ver na aula")
 console.log("Enviando para o Github")
+console.log("Testando o pull")
